2be89ecdf091e8a370f2a757730fe0ec85a5c167
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-05-24 10:20:17 +0800

    Refactored Social.jsx

--- src/Social.jsx
+++ src/Social.jsx
@@ -1,13 +1,18 @@
 import "./Social.css"
 
-function Social({ prop }) {
+/** A component to display the clickable logo and name of a social media link.
+ * 
+ * @param {string} url The url to the social media.
+ * @param {string} imagePath The filepath to the logo.
+ * @param {string} name The name of the social media.
+ * @returns The Social component.
+ */
+function Social({ url, imagePath, name }) {
     return (
-        <>
-            <a className="social" href={prop.link} target="_blank">
-                <img className="logo" src={prop.imagepath} />
-                <p>{prop.text}</p>
-            </a>
-        </>
+        <a className={"social " + name.toLowerCase()} href={url} target="_blank">
+                <img className="logo" src={imagePath} />
+                <p>{name}</p>
+        </a>
     );
 }
 
